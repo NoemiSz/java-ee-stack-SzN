69ab0a2b2decc6f3e21704e399084cbf970784b3
NoemiSz <szokenoemijob@gmail.com>
2019-03-27 13:11:07 +0100

    Stack test for add method is working

--- src/test/java/com/codecool/stack/StackTest.java
+++ src/test/java/com/codecool/stack/StackTest.java
@@ -1,7 +1,25 @@
 package com.codecool.stack;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class StackTest {
+
+    Stack stack = new Stack(2);
+
+
+    @Test
+    public void addToStack(){
+        stack.addToStack("name");
+        assertEquals("name", stack.getStack()[0]);
+    }
+
+    @Test
+    public void getException(){
+        stack.addToStack("name");
+        stack.addToStack("name");
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()-> stack.addToStack("newO"));
+}
+
 
 }
